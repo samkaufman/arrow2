32cb59ecb867b94d5b55f5049609fee1960e650e
Sam Kaufman <emrysk@gmail.com>
2022-07-26 09:42:10 -0700

    FileSink now closes the underlying writer.

--- src/io/parquet/write/sink.rs
+++ src/io/parquet/write/sink.rs
@@ -1,6 +1,6 @@
 use std::{collections::HashMap, pin::Pin, sync::Arc, task::Poll};
 
-use futures::{future::BoxFuture, AsyncWrite, FutureExt, Sink, TryFutureExt};
+use futures::{future::BoxFuture, AsyncWrite, AsyncWriteExt, FutureExt, Sink, TryFutureExt};
 use parquet2::metadata::KeyValue;
 use parquet2::write::FileStreamer;
 use parquet2::write::WriteOptions as ParquetWriteOptions;
@@ -218,6 +218,7 @@ where
 
                     this.task = Some(Box::pin(async move {
                         writer.end(kv_meta).map_err(Error::from).await?;
+                        writer.into_inner().close().map_err(Error::from).await?;
                         Ok(None)
                     }));
                     this.poll_complete(cx)
